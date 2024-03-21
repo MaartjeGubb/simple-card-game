ffbc0434b4b6b9f413e652c0b4fc20dab4c46b5f
Maartje Gubbels <maartje.gubbels@medapp.nl>
2024-03-21 17:13:36 +0100

    Drawing cards from deck works

--- Stok.java
+++ Stok.java
@@ -1,44 +1,46 @@
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Stok {
-    private Kaart[] kaartenInStok;
+    private List<Kaart> kaarten;
+    private int length = 52;
 
     public Stok() {
         creeerStok();
     }
 
+    public int getLength() {
+        return this.length;
+    }
+
+    // Misschien beter om shuffle te doen en dan de bovenste te pakken
+    // length is eigenlijk kaarten.size()??
     public Kaart drawCard() {
-        int rand = new Random().nextInt(51);
-        Kaart getrokkenKaart = kaartenInStok[rand];
+        int rand = new Random().nextInt(this.length);
+        Kaart getrokkenKaart = kaarten.get(rand);
+        kaarten.remove(rand);
+        this.length -= 1;
         return getrokkenKaart;
     }
 
     private void creeerStok() {
-        String[] waarde = {
-                "2", "3", "4", "5", "6", "7", "8", "9", "10",
-                "Boer", "Vrouw", "Koning", "Aas"
+        // WIP even ints van gemaakt voor de eerste versie
+        int[] waarde = {
+                2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14
         };
 
         String[] type = {
                 "Harten", "Klaveren", "Schoppen", "Ruiten"
         };
 
-        int count = 0;
-        kaartenInStok = new Kaart[waarde.length * type.length];
+        this.kaarten = new ArrayList<Kaart>();
         for (int i = 0; i < type.length; i++) {
             for (int j = 0; j < waarde.length; j++) {
-                kaartenInStok[count] = new Kaart(type[i], waarde[j]);
-                count += 1;
+                this.kaarten.add(new Kaart(type[i], waarde[j]));
             }
         }
 
     }
-
-    public static void main(String[] args) {
-        Stok deck = new Stok();
-        Kaart kaartGetrokken = deck.drawCard();
-        System.out.println(kaartGetrokken.getType());
-        System.out.println(kaartGetrokken.getWaarde());
-    }
-
 }
